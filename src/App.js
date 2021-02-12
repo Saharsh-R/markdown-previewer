4fabe6ac71901843e23e832dbfc22b8d137ee289
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-02-12 09:50:11 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Split from 'react-split'
+import marked, { Renderer, use } from 'marked';
+import  { useState, useEffect } from 'react';
+
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://reactjs.org/logo-og.png)
+`;
+
 
 function App() {
+
+  let [mark, setMark] = useState(placeholder)
+
+  const handleChange = e => {
+    setMark(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Split
+        className='split'
+        minSize = {400}
+      >
+          <C1 mark={mark} onChange={handleChange}/>
+          <C2 mark={mark}/>
+      </Split>
+
+      
+    </div>
+  );
+}
+
+function C1(props) {
+  return (
+    <div className="comp">
+      <p className='heading'>Editor</p>
+      <div id='editor-box'>
+        <textarea id='editor' value={props.mark}  onChange={props.onChange}></textarea>
+      </div>
+    </div>
+  );
+}
+
+function C2(props) {
+  
+  return (
+    <div className="comp ">
+      <p className='heading'>Previewer</p>
+      <div id='preview' className ="preview" dangerouslySetInnerHTML={{__html: marked(props.mark, {gfm: true, breaks: true})}}></div>
     </div>
   );
 }
